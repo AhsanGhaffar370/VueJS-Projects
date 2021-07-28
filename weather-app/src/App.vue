446d6a1120f25d041e2923df735b78163dae2446
AhsanRao <35622245+Ahsanrao237@users.noreply.github.com>
2021-07-29 00:10:31 +0500

    create weather app

---
+++ weather-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld :apiKey="api"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+      api: "1380719347e60d9475d99888fcab84d4",
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
